50c6025a8dc49968de88843287e2f2826fed3939
AwesomePotatoCodes <105654727+AwesomePotatoCodes@users.noreply.github.com>
2023-01-14 12:40:12 -0600

    Removed file reading function

--- tests/files.js
+++ tests/files.js
@@ -1,22 +1,9 @@
 const fs = require('fs');
 
-function read(filepath, encoding) {
-  fs.readFile(filepath, encoding, (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    return data;
-  });
-}
-
-// fs.readFile('files/opinion.txt', 'utf8', (err, data) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log(data);
-// });
-
-const contents = read('files/opinions.txt', 'utf8');
-console.log(`File reads "${contents}"`);
+fs.readFile('files/opinion.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
